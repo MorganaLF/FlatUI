ebf1e9784e41a781b4fb2e9a5e2f974d63856e51
Chernenko Viktoriya <4743866@mail.ru>
2018-11-09 14:57:22 +0300

    Настройка Slider Widget

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './wrapper/standart-buttons/buttons.js';
 import css from './styles/main.styl';
 import jQuery from 'jquery';
+import './jQuery-UI/jquery-ui.min'
 import './wrapper/percentages-and-charts/percentage/jquery-progresspiesvg-min'
 import './wrapper/percentages-and-charts/percentage/percentage'
 //import Chartist from 'chartist'
 import './wrapper/percentages-and-charts/pie-chart/jchart'
 import './wrapper/percentages-and-charts/pie-chart/pie-chart'
+import './wrapper/sliders/slider/slider'
 
 console.log(jQuery);
 
---
+++ src/wrapper/sliders/slider/slider.js
@@ -0,0 +1,14 @@
+import $ from 'jquery'
+
+$( function() {
+
+  var handle = $( ".custom-handle" );
+  $( ".slider" ).slider({
+    create: function() {
+      handle.text( $( this ).slider( "value" ) );
+    },
+    slide: function( event, ui ) {
+      handle.text( ui.value );
+    }
+  });
+} );
