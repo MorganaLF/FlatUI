b532f31f343eba7b2ba0e1cdaa0f3ae7f7046166
Chernenko Viktoriya <4743866@mail.ru>
2018-11-22 13:22:09 +0300

    Вынесение Button в отдельный блок

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import './wrapper/standart-buttons/buttons.js';
-import css from './styles/main.styl';
-import jQuery from 'jquery';
+import './wrapper/standart-buttons/button/button.js'
+import css from './styles/main.styl'
+import jQuery from 'jquery'
 import './jQuery-UI/jquery-ui.min'
 import './wrapper/percentages-and-charts/percentage/jquery-progresspiesvg-min'
 import './wrapper/percentages-and-charts/percentage/percentage'
-//import Chartist from 'chartist'
 import './wrapper/percentages-and-charts/pie-chart/jchart'
 import './wrapper/percentages-and-charts/pie-chart/pie-chart'
 import './wrapper/sliders/slider/slider'
@@ -13,8 +12,6 @@ import './wrapper/search-dropdown-user/dropdown/dropdown'
 import './wrapper/location/map'
 import './wrapper/calendar-and-messaging/calendar/calendar'
 import './wrapper/video/video'
-
-console.log(jQuery);
 
 
 
