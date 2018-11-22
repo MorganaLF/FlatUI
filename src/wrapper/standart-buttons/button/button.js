1b94ff5c65236cd08492cca8f4269863e7811ff9
Chernenko Viktoriya <4743866@mail.ru>
2018-11-22 15:28:03 +0300

    Рефакторинг блока Button

--- src/wrapper/standart-buttons/button/button.js
+++ src/wrapper/standart-buttons/button/button.js
@@ -1,33 +1,31 @@
-window.addEventListener('load', function(){
-  let buttonsArr = document.getElementsByClassName('button');
+import $ from 'jquery';
+
+$(function() {
 
   function buttonOnClick (e) {
-    let child = e.target.querySelector('.button__shadow');
+
+    let child = $(e.target).find('.button__shadow');
     let parentCoord = this.getBoundingClientRect();
     let parentTop = parentCoord.top;
     let parentLeft = parentCoord.left;
 
-    child.style.top = e.clientY - parentTop - 25 + 'px';
-    child.style.left = e.clientX - parentLeft - 25 + 'px';
-    child.style.display = 'block';
-
-    console.log(e.clientY, parentTop)
+    child.css('top', e.clientY - parentTop - 25 + 'px');
+    child.css('left', e.clientX - parentLeft - 25 + 'px');
+    child.css('display', 'block');
 
   }
 
-  function buttonRemoveClick (e) {
-    let child = this.querySelector('.button__shadow');
-    setTimeout(function(){
-      child.style.display = 'none';
+  function buttonRemoveClick () {
+    setTimeout(() => {
+      $(this).find('.button__shadow').css('display', 'none');
     }, 50);
   }
 
-  for (let i = 0; i < buttonsArr.length; i++) {
-    buttonsArr[i].addEventListener('mousedown', buttonOnClick);
-    buttonsArr[i].addEventListener('mouseup', buttonRemoveClick);
-  }
+  $('button').each(function(){
+    $(this).on('mousedown', buttonOnClick);
+    $(this).on('mouseup', buttonRemoveClick);
+  });
 });
-
 
 
 
