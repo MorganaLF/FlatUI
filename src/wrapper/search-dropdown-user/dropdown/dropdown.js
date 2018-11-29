14d8ff4a93d90654d6e23d573a054e77fc7f2d9d
Chernenko Viktoriya <4743866@mail.ru>
2018-11-29 16:42:45 +0300

    Доработка блока Dropdown (возможность изменения placeholder)

--- src/wrapper/search-dropdown-user/dropdown/dropdown.js
+++ src/wrapper/search-dropdown-user/dropdown/dropdown.js
@@ -1,20 +1,24 @@
 import $ from 'jquery'
 
 $(document).ready(function() {
-  $('.dropdown').select2({
-    minimumResultsForSearch: Infinity,
-    dropdownCssClass: 'dropdown__option',
-    placeholder: 'Choose An Option'
+  $('.dropdown').each(function () {
+
+    $(this).select2({
+      minimumResultsForSearch: Infinity,
+      dropdownCssClass: 'dropdown__option',
+      placeholder: $(this).data('placeholder')
+    });
+
+    /* Реинициализация для адаптивности */
+
+    $( window ).resize(function() {
+      $(this).select2('destroy')
+          .select2({
+            minimumResultsForSearch: Infinity,
+            dropdownCssClass: 'dropdown__option',
+            placeholder: $(this).data('placeholder')
+          });
+    });
   });
 });
 
-/* Реинициализация для адаптивности */
-
-$( window ).resize(function() {
-  $('.dropdown').select2('destroy')
-                .select2({
-                  minimumResultsForSearch: Infinity,
-                  dropdownCssClass: 'dropdown__option',
-                  placeholder: 'Choose An Option'
-                });
-});
