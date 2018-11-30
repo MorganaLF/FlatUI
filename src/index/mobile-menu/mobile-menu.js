129dbd42f61fd871f7b8dbbbe0aac3183a09e972
Chernenko Viktoriya <4743866@mail.ru>
2018-11-30 17:29:50 +0300

    Responsive верстка страницы Index

--- src/index/mobile-menu/mobile-menu.js
+++ src/index/mobile-menu/mobile-menu.js
@@ -1,9 +1,11 @@
 import $ from 'jquery'
 
 $('.mobile-menu__button').on('click', function () {
-  $('.body-wrapper').toggleClass('body-wrapper_opened')
+  $('.body-wrapper').toggleClass('body-wrapper_opened');
+  $('.mobile-menu').toggleClass('mobile-menu_opened');
 });
 
 $(window).resize(function () {
   $('.body-wrapper').removeClass('body-wrapper_opened');
+  $('.mobile-menu').removeClass('mobile-menu_opened');
 });
