9a87aae3d87e1f3fdf64733f37af510df2333976
Chernenko Viktoriya <4743866@mail.ru>
2018-12-04 20:59:53 +0300

    Исправление ошибки при загрузке скрипта player.js

--- src/wrapper/video/player/player.js
+++ src/wrapper/video/player/player.js
@@ -1,94 +1,102 @@
 import $ from 'jquery'
 
 $( function() {
-  let durarion;
-  let time_update_interval;
-  let video = $('.player').get(0);
-  let playButton = $('.player__play-button');
 
-  /* Воспроизведение видео */
+  $('.player').each(function () {
+    let video = $(this)[0];
+    let playButton = $(video).closest('.player__wrapper').find('.player__play-button');
+    let fullButton = $(video).closest('.player__wrapper').find('.player__full-button');
+    let progressLine = $(video).closest('.player__wrapper').find('.player__progress');
+    let fullProgressLine = $(video).closest('.player__wrapper').find('.player__progress--full');
+    let durarion;
+    let time_update_interval;
 
-  function playVideo () {
-    if (video.paused) {
-      $('.player__play-button').addClass('player__play-button_playing');
-      video.play();
-      time_update_interval = setInterval(function () {
-        updateProgressBar();
-      }, 1000);
-    } else {
-      $('.player__play-button').removeClass('player__play-button_playing');
-      video.pause();
-      clearInterval(time_update_interval);
+    /* Воспроизведение видео */
+
+    function playVideo () {
+      if (video.paused) {
+        $(playButton).addClass('player__play-button_playing');
+        video.play();
+        time_update_interval = setInterval(function () {
+          updateProgressBar();
+        }, 1000);
+      } else {
+        $(playButton).removeClass('player__play-button_playing');
+        video.pause();
+        clearInterval(time_update_interval);
+      }
     }
-  }
-  $('.player').click(playVideo);
-  playButton.click(playVideo);
+    $(video).click(playVideo);
+    playButton.click(playVideo);
 
-  /* Развернуть на весь экран */
-          let isFull = false;
-          function playFullscreen (){
+    /* Развернуть на весь экран */
+    let isFull = false;
 
-           var doc = document.querySelector('.player__wrapper');
+    function playFullscreen (){
 
-          if (!isFull) {
-              document.querySelector('.player__full-button').classList.add('player__full-button_opened');
-              if (doc.requestFullscreen) {
-                doc.requestFullscreen();
-              }
-              else if (doc.mozRequestFullScreen) {
-                doc.mozRequestFullScreen();
-              }
-              else if (doc.webkitRequestFullScreen) {
-               doc.webkitRequestFullScreen();
-              }
-              else if (doc.msRequestFullscreen) {
-                doc.msRequestFullscreen();
-              }
-            } else {
-              document.querySelector('.player__full-button').classList.remove('player__full-button_opened');
-              if (document.exitFullscreen) {
-                document.exitFullscreen();
-              }
-               else if (document.mozCancelFullScreen) {
-                  document.mozCancelFullScreen();
-               }
-              else if (document.webkitCancelFullScreen) {
-                document.webkitCancelFullScreen();
-              }
-               else if (document.msExitFullscreen) {
-                 document.msExitFullscreen();
-               }
-            }
+      let doc = $(video).closest('.player__wrapper')[0]; /* Исправить? */
 
-            isFull = !isFull;
-          }
+      if (!isFull) {
+        $(fullButton).addClass('player__full-button_opened');
+        if (doc.requestFullscreen) {
+          doc.requestFullscreen();
+        }
+        else if (doc.mozRequestFullScreen) {
+          doc.mozRequestFullScreen();
+        }
+        else if (doc.webkitRequestFullScreen) {
+          doc.webkitRequestFullScreen();
+        }
+        else if (doc.msRequestFullscreen) {
+          doc.msRequestFullscreen();
+        }
+      } else {
+        $(fullButton).removeClass('player__full-button_opened');
 
-  $('.player__full-button').click(playFullscreen);
+        if (document.exitFullscreen) {
+          document.exitFullscreen();
+        }
+        else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+        }
+        else if (document.webkitCancelFullScreen) {
+          document.webkitCancelFullScreen();
+        }
+        else if (document.msExitFullscreen) {
+          document.msExitFullscreen();
+        }
+      }
+
+      isFull = !isFull;
+    }
+
+    $(fullButton).click(playFullscreen);
 
 
-  /* Линия прогресса */
-         function progress (event) {
+    /* Линия прогресса */
+    function progress (event) {
 
-             let line_width = document.querySelector('.player__progress').clientWidth;
-             // положение элемента
-             //var pos = document.getElementById('line').offset();
-             let elem_left = document.querySelector('.player__progress').getBoundingClientRect().left;
-             // положение курсора внутри элемента
-             let Xinner = event.pageX - elem_left;
-             let newTime = video.duration * (Xinner / line_width);
-             // Skip video to new time.
-             video.currentTime = newTime;
-             updateProgressBar();
-         }
+      let line_width = $(progressLine).width();
+      // положение элемента
+      let elem_left = $(progressLine).offset().left;
+      // положение курсора внутри элемента
+      let Xinner = event.pageX - elem_left;
+      let newTime = video.duration * (Xinner / line_width);
+      // Skip video to new time.
+      video.currentTime = newTime;
+      updateProgressBar();
+    }
 
-  // Обновляем прогресс
-         function updateProgressBar(){
-             let line_width = document.querySelector('.player__progress').clientWidth;
-             let persent = (video.currentTime / video.duration);
-             document.querySelector('.player__progress--full').style.width = persent * line_width + 'px';
-         }
+    // Обновляем прогресс
+    function updateProgressBar(){
+      let line_width = $(progressLine).width();
+      let persent = (video.currentTime / video.duration);
+      $(fullProgressLine).css('width', persent * line_width + 'px');
+    }
 
-      document.querySelector('.player__progress').addEventListener('click', progress);
+    $(progressLine).on('click', progress);
+
+  });
 
 
 });
