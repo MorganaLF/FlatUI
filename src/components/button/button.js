381dd6337c03638131966486b54e8a5ec9e35b67
Chernenko Viktoriya <4743866@mail.ru>
2019-03-27 22:45:17 +0300

    Замена функции блока button на класс.

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,44 +1,60 @@
 import $ from 'jquery';
 
-$(function() {
+class Button {
+  constructor(element) {
+    this.element = element;
+  }
 
-  function buttonOnClick (e) {
-    e.preventDefault();
-
-    let ink, d, x, y;
-
-    if ($(this).find(".button__ripple").length === 0)
-      $(this).prepend("<span class='button__ripple'></span>");
-
-    ink = $(this).find(".button__ripple");
-    ink.removeClass("button__ripple_animated");
-
-    if(!ink.height() && !ink.width())
-    {
-      d = Math.max($(this).outerWidth(), $(this).outerHeight());
-      ink.css({height: d, width: d});
+  createRipple(e, $this) {
+    if ($this.find('.button__ripple').length === 0) {
+      $this.prepend("<span class='button__ripple'></span>");
     }
 
-    x = e.pageX - $(this).offset().left - ink.width()/2;
-    y = e.pageY - $(this).offset().top - ink.height()/2;
+    const ripple = $this.find('.button__ripple');
+    ripple.removeClass('button__ripple_animated');
 
-    ink.css({top: y+'px', left: x+'px'}).addClass("button__ripple_animated");
-
-    if ($(this).attr('href')) {
-      let link = $(this).attr('href');
-      setTimeout(function() { window.location.href = link; }, 650);
+    if (!ripple.height() && !ripple.width()) {
+      const rippleDiameter = Math.max($this.outerWidth(), $this.outerHeight());
+      ripple.css({ height: rippleDiameter, width: rippleDiameter });
     }
 
-    if($(this).attr('type') === 'submit') {
-      let form = $(this).closest('form');
-      setTimeout(function() { form.submit() }, 650);
+    const rippleXCoord = e.pageX - $this.offset().left - ripple.width() / 2;
+    const rippleYCoord = e.pageY - $this.offset().top - ripple.height() / 2;
+
+    ripple
+      .css({ top: `${rippleYCoord}px`, left: `${rippleXCoord}px` })
+      .addClass('button__ripple_animated');
+  }
+
+  restoreDefault($this) {
+    if ($this.attr('href')) {
+      const link = $this.attr('href');
+      setTimeout(() => {
+        window.location.href = link;
+      }, 650);
+    }
+
+    if ($this.attr('type') === 'submit') {
+      const form = $this.closest('form');
+      setTimeout(() => {
+        form.submit();
+      }, 650);
     }
   }
 
-  $('.button').each(function(){
-    $(this).on('click', buttonOnClick);
-  });
-});
+  callListeners(e) {
+    e.preventDefault();
 
+    const $this = $(e.target);
 
+    this.createRipple(e, $this);
+    this.restoreDefault($this);
+  }
 
+  addListener() {
+    this.element.on('click', this.callListeners.bind(this));
+  }
+}
+
+const button = new Button($('.button'));
+button.addListener();
