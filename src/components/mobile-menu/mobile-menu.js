4cdc9fb97e3d4dc470c2f255e38499b7a19d18c8
Chernenko Viktoriya <4743866@mail.ru>
2019-03-29 23:49:24 +0300

    Рефакторинг в соответствии со стандартами блока Mobile-menu.

--- src/components/mobile-menu/mobile-menu.js
+++ src/components/mobile-menu/mobile-menu.js
@@ -1,11 +1,43 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
-$('.mobile-menu__button').on('click', function () {
-  $('.body-wrapper').toggleClass('body-wrapper_opened');
-  $('.mobile-menu').toggleClass('mobile-menu_opened');
-});
+class MobileMenu {
+  constructor(element, elementIndex) {
+    this.$element = element;
+    this.elementIndex = elementIndex;
+    this.$body = null;
+    this.$menu = null;
+    this.init();
+  }
 
-$(window).resize(function () {
-  $('.body-wrapper').removeClass('body-wrapper_opened');
-  $('.mobile-menu').removeClass('mobile-menu_opened');
-});
+  init() {
+    this._addEventListeners();
+  }
+
+  toggleMobileMenu() {
+    if (this.$body === null) this.$body = $('.js-body-wrapper');
+    if (this.$menu === null) this.$menu = $('.js-mobile-menu');
+    this.$body.toggleClass('body-wrapper_opened');
+    this.$menu.toggleClass('mobile-menu_opened');
+  }
+
+  hideMobileMenu() {
+    if (this.$body === null) this.$body = $('.js-body-wrapper');
+    if (this.$menu === null) this.$menu = $('.js-mobile-menu');
+    this.$body.removeClass('body-wrapper_opened');
+    this.$menu.removeClass('mobile-menu_opened');
+  }
+
+  _addEventListeners() {
+    const $window = $(window);
+    $window.on(`resize.mobileMenuHide${this.elementIndex}`, this.hideMobileMenu.bind(this));
+
+    this.$element.on(`click.mobileMenuToggle${this.elementIndex}`, this.toggleMobileMenu.bind(this));
+  }
+}
+
+function createMobileMenuInstance(index) {
+  new MobileMenu($(this), index);
+}
+
+const $mobileMenuButton = $('.js-mobile-menu__button');
+$mobileMenuButton.each(createMobileMenuInstance);
