8f941de8f62df644c63aa18b2336134435dca74a
Chernenko Viktoriya <4743866@mail.ru>
2019-03-29 15:21:03 +0300

    Рефакторинг в соответствии со стандартами блока Calendar.

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,69 +1,65 @@
 import $ from 'jquery';
 
-
 class Calendar {
   constructor(element, elementIndex) {
     this.$element = element;
     this.elementIndex = elementIndex;
-    this.$currentElement = null;
     this.$elementParent = null;
-    this.eventName = null;
     this.init();
   }
 
   init() {
-    this.initDatepickerPlugin();
-    this.displayCurrentDay();
-    this.cutTitleSpaces();
-    this.setResponsiveFontSize();
-    this.addEventListeners();
+    this._initDatepickerPlugin();
+    this._displayCurrentDay();
+    this._cutTitleSpaces();
+    this._setResponsiveFontSize();
+    this._addEventListeners();
   }
 
-  initDatepickerPlugin() {
+  _initDatepickerPlugin() {
     this.$element.datepicker({
       firstDay: 0,
       dayNamesMin: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
     });
   }
 
-  addEventListeners() {
-    $(window).resize(() => {
-      this.setResponsiveFontSize();
-    });
+  _addEventListeners() {
+    const $window = $(window);
+    $window.on(`resize.resizeFont${this.elementIndex}`, this._setResponsiveFontSize.bind(this));
 
     const $arrowButton = this.$element.find('.ui-corner-all');
 
     $arrowButton
-      .on('click', this.setResponsiveFontSize.bind(this))
-      .on('click', this.cutTitleSpaces.bind(this));
+      .on(`click.resizeFont${this.elementIndex}`, this._setResponsiveFontSize.bind(this))
+      .on(`click.cutSpaces${this.elementIndex}`, this._cutTitleSpaces.bind(this));
 
     const $dayButton = this.$element.find('td[data-event="click"]');
 
     $dayButton
-      .on('click', this.setResponsiveFontSize.bind(this))
-      .on('click', this.cutTitleSpaces.bind(this));
+      .on(`click.resizeFont${this.elementIndex}`, this._setResponsiveFontSize.bind(this))
+      .on(`click.cutSpaces${this.elementIndex}`, this._cutTitleSpaces.bind(this));
   }
 
-  displayCurrentDay() {
-    this.$elementParent = this.$element.closest('.calendar');
-    const currentDate = this.$element.datepicker('getDate').getDate();
-    const $calendarHeader = this.$elementParent.find('.calendar__header');
-    $calendarHeader.text(currentDate);
+  _displayCurrentDay() {
+    this.$elementParent = this.$element.closest('.js-calendar');
+    const currentDay = this.$element.datepicker('getDate').getDate();
+    const $calendarHeader = this.$elementParent.find('.js-calendar__header');
+    $calendarHeader.text(currentDay);
   }
 
-  cutTitleSpaces() {
-    const $dayButton = this.$element.find('td[data-event="click"]');
-    $dayButton.on('click', this.cutTitleSpaces.bind(this));
-
+  _cutTitleSpaces() {
     const $title = this.$element.find('.ui-datepicker-title');
     const str = $title.html().replace(/&nbsp;/g, '');
     $title.html(str);
+
+    const $dayButton = this.$element.find('td[data-event="click"]');
+    $dayButton.on(`click.cutSpaces${this.elementIndex}`, this._cutTitleSpaces.bind(this));
   }
 
-  setResponsiveFontSize() {
+  _setResponsiveFontSize() {
     const width = this.$element.width();
 
-    const $header = this.$elementParent.find('.calendar__header');
+    const $header = this.$elementParent.find('.js-calendar__header');
     $header.css('font-size', width / 3.5);
 
     const $title = this.$element.find('.ui-datepicker-title');
@@ -72,14 +68,14 @@ class Calendar {
     const $tableHead = this.$element.find('thead');
     $tableHead.css('font-size', width / 31.1);
 
-    const $footer = this.$elementParent.find('.calendar__footer');
+    const $footer = this.$elementParent.find('.js-calendar__footer');
     $footer.css('font-size', width / 21.5);
 
     const $dayLink = this.$element.find('.ui-state-default');
     $dayLink.css('font-size', width / 14.5);
 
     const $dayButton = this.$element.find('td[data-event="click"]');
-    $dayButton.on('click', this.setResponsiveFontSize.bind(this));
+    $dayButton.on(`click.resizeFont${this.elementIndex}`, this._setResponsiveFontSize.bind(this));
   }
 }
 
@@ -87,56 +83,5 @@ function createCalendarInstance(index) {
   new Calendar($(this), index);
 }
 
-$('.calendar__body').each(createCalendarInstance);
-
-/* Инициализация */
-
-// $('.calendar__body').each(function () {
-//   const calendar = $(this);
-//
-//   calendar.datepicker({
-//     firstDay: 0,
-//     dayNamesMin: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-//   });
-//
-//   /* Вывод числа в шапке */
-//
-//   function cutTitleSpaces() {
-//     $(calendar).find('td[data-event="click"]').on('click', cutTitleSpaces);
-//     const str = $(calendar).find('.ui-datepicker-title').html().replace(/&nbsp;/g, '');
-//     $(calendar).find('.ui-datepicker-title').html(str);
-//   }
-//
-//   cutTitleSpaces();
-//
-//   const currentDate = $(calendar).datepicker('getDate').getDate();
-//   $(calendar).closest('.calendar').find('.calendar__header').text(currentDate);
-//
-//   /* Адаптивный шрифт */
-//   let width;
-//   function setResponsiveFontSize() {
-//     width = $(calendar).find('.ui-widget.ui-widget-content').width();
-//     $(calendar).closest('.calendar').find('.calendar__header').css('font-size', width / 3.5);
-//     $(calendar).find('.ui-datepicker .ui-datepicker-title').css('font-size', width / 11.6);
-//     $(calendar).find('.ui-datepicker-calendar thead').css('font-size', width / 31.1);
-//     $(calendar).closest('.calendar').find('.calendar__footer').css('font-size', width / 21.5);
-//     $(calendar).find('.ui-datepicker td span, .ui-datepicker td a').css('font-size', width / 14.5);
-//     $(calendar).find('td[data-event="click"]').on('click', setResponsiveFontSize);
-//   }
-//
-//   setResponsiveFontSize();
-//
-//   /* Адаптивный шрифт при ресайзе окна */
-//
-//   $(window).resize(() => {
-//     setResponsiveFontSize();
-//   });
-//
-//   /* Расширение функции плагина, установка размера шрифта при перерисовке */
-//
-//   $(calendar).find('.ui-corner-all').on('click', setResponsiveFontSize);
-//   $(calendar).find('.ui-corner-all').on('click', cutTitleSpaces);
-//
-//   $(calendar).find('td[data-event="click"]').on('click', setResponsiveFontSize);
-//   $(calendar).find('td[data-event="click"]').on('click', cutTitleSpaces);
-// });
+const $calendar = $('.js-calendar__body');
+$calendar.each(createCalendarInstance);
