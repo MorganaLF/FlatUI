2891fe4a202df6ced67654de758822a37e1ca0cb
Chernenko Viktoriya <4743866@mail.ru>
2019-04-03 13:20:23 +0300

    Рефакторинг в соответствии с стандартами блока Stage.

--- src/components/stage/stage.js
+++ src/components/stage/stage.js
@@ -1,54 +1,107 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
-$('.stage__list').each(function () {
-  let stage = $(this);
-  let containerWidth = $(stage).width();
-  let currentIndex;
+class Stage {
+  constructor(element, elementIndex) {
+    this.$stage = element;
+    this.stageIndex = elementIndex;
+    this.$stageItem = null;
+    this.$stageContent = null;
+    this.containerWidth = 0;
+    this.currentStageItemIndex = 0;
+    this.init();
+  }
 
-  $(stage).closest('.stage').find('.stage__content').eq(0).css('width', containerWidth * $('.stage__item').length + 'px')
-  let contentHeight = $('.stage__item').closest('.stage').find('.stage__content>*').eq(0).outerHeight();
-  $('.stage__item').closest('.stage').find('.stage__content').eq(0).css('height', contentHeight);
+  init() {
+    this._findElements();
+    this._setContainerWidth();
+    this._setContentHeight();
+    this._addEventListeners();
+  }
 
+  setActiveStage(currentIndex) {
+    this.currentStageItemIndex = currentIndex;
 
+    this.$stageItem.each((index, item) => {
+      const inactiveItemIndex = index > currentIndex;
 
-  $(window).resize(function () {
-    let containerWidth = $(stage).width();
-    $(stage).closest('.stage').find('.stage__content').eq(0).css('width', containerWidth * $('.stage__item').length + 'px')
-
-  });
-
-  $(stage).find('.stage__item').each(function (index) {
-    let stageItem = $(this);
-
-    $(stageItem).on('click', function(){
-      currentIndex = index;
-      for (let i = 0; i <= index; i++){
-        $(stageItem).closest('.stage__list').find('.stage__item').eq(i).removeClass('stage__item_inactive')
+      if (inactiveItemIndex) {
+        $(item).addClass('stage__item_inactive');
+      } else {
+        $(item).removeClass('stage__item_inactive');
       }
-      for (let i = 0; i < index; i++){
-        $(stageItem).closest('.stage__list').find('.stage__line').eq(i).removeClass('stage__line_inactive')
-      }
-      for (let i = index + 1; i <= $('.stage__item').length; i++) {
-        $(stageItem).closest('.stage').find('.stage__item').eq(i).addClass('stage__item_inactive')
-      }
-      for (let i = index; i < $('.stage__item').length; i++) {
-        $(stageItem).closest('.stage').find('.stage__line').eq(i).addClass('stage__line_inactive')
-      }
-      let contentHeight = $(stageItem).closest('.stage').find('.stage__content>*').eq(index).outerHeight();
-      $(stageItem).closest('.stage').find('.stage__content').eq(0).css('transform', 'translateX(-' + containerWidth * index + 'px)');
-      $(stageItem).closest('.stage').find('.stage__content').eq(0).css('height', contentHeight)
     });
 
-    $(window).resize(function () {
-      let containerWidth = $(stage).width();
-      let contentHeight = $(stageItem).closest('.stage').find('.stage__content>*').eq(currentIndex).outerHeight();
-      $(stageItem).closest('.stage').find('.stage__content').eq(0).css('height', contentHeight);
-      $(stageItem).closest('.stage').find('.stage__content').eq(0).css('transform', 'translateX(-' + containerWidth * currentIndex + 'px)');
-      $(stageItem).on('click', function(){
-        $(stageItem).closest('.stage').find('.stage__content').eq(0).css('transform', 'translateX(-' + containerWidth * index + 'px)')
-      });
+    const $stageLine = this.$stage.find('.js-stage__line');
+
+    $stageLine.each((index, line) => {
+      const inactiveLineIndex = index >= currentIndex;
+
+      if (inactiveLineIndex) {
+        $(line).addClass('stage__line_inactive');
+      } else {
+        $(line).removeClass('stage__line_inactive');
+      }
     });
 
-  });
+    this.scrollCarousel();
+  }
 
+  scrollCarousel(withTimeout) {
+    this._setContainerWidth();
+
+    this.$stageContent
+      .css('transform', `translateX(-${this.containerWidth * this.currentStageItemIndex}px)`);
+
+    if (withTimeout) {
+      setTimeout(() => {
+        this._setContentHeight();
+      }, 1000);
+    } else {
+      this._setContentHeight();
+    }
+  }
+
+  _addEventListeners() {
+    this.$stageItem.each((index, item) => {
+      $(item).on(
+        `click.StageSetActive${this.stageIndex}`,
+        this.setActiveStage.bind(this, index),
+      );
+    });
+
+    const $window = $(window);
+
+    $window.on(
+      `resize.StageScroll${this.stageIndex}`,
+      this.scrollCarousel.bind(this, true),
+    );
+  }
+
+  _findElements() {
+    this.$stageItem = this.$stage.find('.js-stage__item');
+    this.$stageContent = this.$stage.find('.js-stage__content');
+  }
+
+  _setContainerWidth() {
+    this.containerWidth = this.$stage.width();
+    this.$stageContent.css('width', `${this.containerWidth * this.$stageItem.length}px`);
+  }
+
+  _setContentHeight() {
+    const $currentSlide = this.$stage
+      .find('.js-stage__content>*')
+      .eq(this.currentStageItemIndex);
+
+    const contentHeight = $currentSlide.outerHeight();
+
+    this.$stageContent.css('height', contentHeight);
+  }
+}
+
+$(() => {
+  const $stage = $('.js-stage');
+
+  $stage.each((index, item) => {
+    new Stage($(item), index);
+  });
 });
