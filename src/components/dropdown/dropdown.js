a46f6431e7bb03b609454537f3067b8bb0f74f7d
Chernenko Viktoriya <4743866@mail.ru>
2019-04-24 22:16:13 +0300

    БЭМ #6. Правки блока dropdown.

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -32,7 +32,7 @@ class Dropdown {
 }
 
 $(() => {
-  const $dropdown = $('.js-dropdown');
+  const $dropdown = $('.js-dropdown__select');
 
   $dropdown.each((index, item) => {
     new Dropdown($(item), index);
