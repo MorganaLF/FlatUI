d9c1a55775f93c90f72d3011abc81afef3cd24f1
Chernenko Viktoriya <4743866@mail.ru>
2019-05-12 12:37:38 +0300

    Cleanup #18.

---
+++ src/components/ui-kit-components/dropdown/dropdown.js
@@ -0,0 +1,40 @@
+import 'select2/dist/js/select2.full.min';
+
+class Dropdown {
+  constructor(element, elementIndex) {
+    this.$element = element;
+    this.elementIndex = elementIndex;
+    this.init();
+  }
+
+  init() {
+    this._initSelectPlugin();
+    this._addEventListeners();
+  }
+
+  _initSelectPlugin() {
+    this.$element.select2({
+      minimumResultsForSearch: Infinity,
+      dropdownCssClass: 'dropdown__option',
+      placeholder: this.$element.data('placeholder'),
+    });
+  }
+
+  _addEventListeners() {
+    const $window = $(window);
+    $window.on(`resize.dropdownResize${this.elementIndex}`, this._handleWindowResize.bind(this));
+  }
+
+  _handleWindowResize() {
+    this.$element.select2('destroy');
+    this._initSelectPlugin();
+  }
+}
+
+$(() => {
+  const $dropdown = $('.js-dropdown__select');
+
+  $dropdown.each((index, item) => {
+    new Dropdown($(item), index);
+  });
+});
